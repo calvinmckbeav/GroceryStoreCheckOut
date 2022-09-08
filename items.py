49581f377cd776230bc1a5600d666d7b46ac05cc
calvinmckbeav <92180625+calvinmckbeav@users.noreply.github.com>
2022-09-08 00:35:10 -0400

    Create items.py

---
+++ items.py
@@ -0,0 +1,13 @@
+# available products to the customer
+products = {
+  "apple" : 1.50,
+  "bananas" : 2.00,
+  "pear" : 3.25,
+  "grapes" : 1.75,
+  "watermelon" : 4.50,
+  "kiwi" : 3.00,
+  "strawberries" : 2.50,
+}
+
+# items the customer has added to their order
+selected = []
